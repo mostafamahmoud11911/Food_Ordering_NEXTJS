db2a706bfb838ac63f7e05417a2b6f3d892682be
mostafamahmoud11911 <mm0766633@gmail.com>
2025-04-23 01:18:02 +0400

    fix prisma

--- src/app/[locale]/admin/menu-items/[productId]/edit/page.tsx
+++ src/app/[locale]/admin/menu-items/[productId]/edit/page.tsx
@@ -1,25 +1,25 @@
 import { Pages, Routes } from '@/constants/enums';
 import { Locale } from '@/i18n.config';
-import { getProduct, getProducts } from '@/server/db/products'
-import { redirect } from 'next/navigation';
+import { getProduct } from '@/server/db/products'
+import { notFound, redirect } from 'next/navigation';
 import React from 'react'
 import Form from '../../_components/Form';
 import { getTrans } from '@/lib/translate';
 import { getCategories } from '@/server/db/categories';
 
-export async function generateStaticParams() {
-  const products = await getProducts();
-  return products.map(product => ({ productId: product.id }))
-}
+
 
 
 export default async function EditProduct({ params }: { params: Promise<{ locale: Locale, productId: string }> }) {
 
   const { locale, productId } = (await params);
+  if (!productId) {
+    return notFound(); // Returns 404
+  }
   const translations = await getTrans(locale);
   const product = await getProduct(productId);
   const category = await getCategories()
-  if(!product) {
+  if (!product) {
     redirect(`/${locale}/${Routes.ADMIN}/${Pages.MENU_ITEMS}`);
   }
 
@@ -30,7 +30,7 @@ export default async function EditProduct({ params }: { params: Promise<{ locale
     <main>
       <section>
         <div className='container'>
-          <Form translations={translations!} product={product} categories={category}/>
+          <Form translations={translations!} product={product} categories={category} />
         </div>
       </section>
     </main>
